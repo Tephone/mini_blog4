b8e0355e3961db5f09b08a1dd9d2459821b7c470
naoko kasahara <kassa0918@gmail.com>
2021-07-08 20:26:38 +0900

    フォロー機能アソシエーション、relations_controller生成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,13 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :blogs, dependent: :destroy
-  validates :profile, length: {maximum: 200}
+  validates :profile, length: {maximum: 200} 
   validates :name, presence: true,
                     length: {maximum: 20},
-                    format: {with: /\A[a-zA-Z]+\z/}
+                    format:{with: /\A[a-zA-Z]+\z/}
+  has_many :blogs, dependent: :destroy
+  has_many :active_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+  has_many :passive_relationships, foreign_key: 'followed_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followings, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower 
 end
