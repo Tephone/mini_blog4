6af8f5a33f8cce87ca14fe91c44f2d9dd0cf3efe
naoko kasahara <kassa0918@gmail.com>
2021-07-08 19:39:18 +0900

    validation, passwordなしで編集可能に

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :blogs, dependent: :destroy
+  validates :profile, length: {maximum: 200}
+  validates :name, presence: true,
+                    length: {maximum: 20},
+                    format: {with: /\A[a-zA-Z]+\z/}
 end
