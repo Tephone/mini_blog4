2415e26644fa733a5a94f17f99a21bea6246346d
naoko kasahara <kassa0918@gmail.com>
2021-07-12 02:03:11 +0900

    画像投稿機能追加

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,5 @@
 class Blog < ApplicationRecord
+  mount_uploader :image, ImageUploader
   belongs_to :user
   has_many :likes, dependent: :destroy
   has_many :users_like, through: :likes, source: :user
