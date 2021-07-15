546f496a1361d751790f81a9d363a474a2631974
naoko kasahara <kassa0918@gmail.com>
2021-07-15 19:10:54 +0900

    comment機能アソシエーション

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :users, only: %i[index show]
   root 'blogs#index'
   resources :blogs, only: %i[index new create]
-  resources :relationships, only: %i[create destroy]
+	resources :relationships, only: %i[create destroy]
+	resources :comments, only: %i[create destroy]
 end
